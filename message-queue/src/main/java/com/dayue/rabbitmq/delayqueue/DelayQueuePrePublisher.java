7b416eacae2b723e49d573be2dd94a9b15324008
zhengdayue <email>
2021-03-11 14:40:25 +0800

    feat:完善延迟队列,发送消息时动态设置延迟消费时间

--- message-queue/src/main/java/com/dayue/rabbitmq/delayqueue/DelayQueuePrePublisher.java
+++ message-queue/src/main/java/com/dayue/rabbitmq/delayqueue/DelayQueuePrePublisher.java
@@ -26,7 +26,12 @@ public class DelayQueuePrePublisher {
     public void sendMsg(Order order) {
         try {
             //设置延迟队列交换机、延迟队列路由键,消息实体并且发送消息
-            rabbitTemplate.convertAndSend(RabbitMqConstants.DELAY_EXCHANGE_PRE, RabbitMqConstants.DELAY_ROUTING_KEY_PRE, order);
+            //rabbitTemplate.convertAndSend(RabbitMqConstants.DELAY_EXCHANGE_PRE, RabbitMqConstants.DELAY_ROUTING_KEY_PRE, order);
+            //消息延迟时间设置为10秒,队列的x-message-ttl是设置30秒,以最小时间为准,即发送消息时的10秒
+            rabbitTemplate.convertAndSend(RabbitMqConstants.DELAY_EXCHANGE_PRE, RabbitMqConstants.DELAY_ROUTING_KEY_PRE, order,message -> {
+                message.getMessageProperties().setExpiration("10000");
+                return message;
+            });
             log.info("延迟队列消息发送成功,消息：{},发送时间：{}", order, LocalDateTime.now());
         } catch (Exception e) {
             log.error("延迟队列消息发送异常,消息：{},异常e：", order, e);
