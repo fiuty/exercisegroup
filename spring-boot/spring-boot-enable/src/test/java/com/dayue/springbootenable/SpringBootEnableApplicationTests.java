eb6212d852c28a83e0179a2bce94e6adc122e1dd
zdy <1125450960@qq.com>
2021-04-18 12:43:37 +0800

    feat:提交enable模块代码

---
+++ spring-boot/spring-boot-enable/src/test/java/com/dayue/springbootenable/SpringBootEnableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayue.springbootenable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEnableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
