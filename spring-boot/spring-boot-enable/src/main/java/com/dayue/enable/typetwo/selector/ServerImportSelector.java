af432ee6b4d2aaccc00ba7c5cf0885bf38c4e823
zdy <1125450960@qq.com>
2021-04-18 16:42:27 +0800

    feat:补充注释等信息

--- spring-boot/spring-boot-enable/src/main/java/com/dayue/enable/typetwo/selector/ServerImportSelector.java
+++ spring-boot/spring-boot-enable/src/main/java/com/dayue/enable/typetwo/selector/ServerImportSelector.java
@@ -19,14 +19,17 @@ public class ServerImportSelector implements ImportSelector {
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        //获取注解@EnableActionServer元信息
         Map<String, Object> annotationAttributes = importingClassMetadata.getAnnotationAttributes(EnableActionServer.class.getName());
         assert annotationAttributes != null;
+        //通过获取的注解@EnableActionServer元信息，取定义好的方法属性，并把对象Object强制转换为ActionType枚举类型
         ActionType serverType = (ActionType) annotationAttributes.get("serverType");
-        log.info("getServerType(serverType):{}", getServerType(serverType));
+        log.info("serverType：{}，getServerType(serverType):{}", serverType, getServerType(serverType));
+        //最后返回要选择的类名，注册为spring bean组件
         return new String[]{getServerType(serverType)};
     }
 
-
+    //封装一层，通过枚举值返回不同的类名
     public String getServerType(ActionType serverType) {
         Map<ActionType, String> serverClassMap; serverClassMap = new HashMap<>(2);
         serverClassMap.put(ActionType.TEACHER, TeacherActionServerImpl.class.getName());
--- spring-boot/spring-boot-enable/src/main/java/com/dayue/springbootenable/SpringBootEnableApplication.java
+++ spring-boot/spring-boot-enable/src/main/java/com/dayue/springbootenable/SpringBootEnableApplication.java
@@ -1,11 +1,14 @@
 package com.dayue.springbootenable;
 
+import com.dayue.enable.typeone.EnableHelloWorld;
 import com.dayue.enable.typetwo.annotations.EnableActionServer;
+import com.dayue.enable.typetwo.enums.ActionType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableActionServer
+@EnableHelloWorld
+@EnableActionServer(serverType = ActionType.STUDENT)
 public class SpringBootEnableApplication {
 
     public static void main(String[] args) {
