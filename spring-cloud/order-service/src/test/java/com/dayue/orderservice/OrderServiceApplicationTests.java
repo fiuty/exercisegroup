08aa3bba6e34e76596e64077b948db00057db12c
zdy <email>
2021-08-10 18:44:25 +0800

    feat:添加服务

---
+++ spring-cloud/order-service/src/test/java/com/dayue/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayue.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-cloud/user-service/src/main/java/com/dayue/userservice/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.dayue.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+//组合注解@SpringCloudApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
